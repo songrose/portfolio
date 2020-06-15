f21fcd24b55d9bec94bb9e7d69decee579f25735
rose <sooominsong@gmail.com>
2020-06-15 06:16:34 -0700

    Updates

---
+++ precache-manifest.7bc8a482ffd7442e5474962a8fcacd60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e2b21877373e4efcb72ef4c16ddb6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cbdb20357da12d3f16f0",
+    "url": "/portfolio/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "e275e949918ee98dcb8e",
+    "url": "/portfolio/static/css/main.efddbccd.chunk.css"
+  },
+  {
+    "revision": "cbdb20357da12d3f16f0",
+    "url": "/portfolio/static/js/2.874b70ad.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/portfolio/static/js/2.874b70ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e275e949918ee98dcb8e",
+    "url": "/portfolio/static/js/main.39e26c1e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
